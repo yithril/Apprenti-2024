34c4d3af67463132b2d58d7784b083c3e2108743
yithril <yithril@gmail.com>
2024-09-10 08:59:47 -0400

    example

---
+++ Week1/September9th/index.js
@@ -0,0 +1,30 @@
+//This is a comment
+let fullName = "Jonathan Hop";
+
+/* 
+This is a multi line comment
+As long as its in between the star and slash
+its a comment and the computer doesnt care.
+*/
+
+console.log(fullName);
+
+//other variable types
+let age = 40;
+
+let pi = 3.14;
+
+let population = 7_000_000_000;
+
+// + - * /
+let students = 10 + 40;
+
+let multiply = 5 * 25;
+
+let isPremiumMember = true;
+let isRewardsMember = false;
+
+// thisIsCamelCase
+// PascalCase ThisIsPascalCase
+//this-is-kebab-case
+//this_is_snake_case
