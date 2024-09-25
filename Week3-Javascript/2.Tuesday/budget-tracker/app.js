2e4baf792024b1c01aedd2c6fc589555f965a353
yithril <yithril@gmail.com>
2024-09-25 10:05:16 -0400

    stuff

--- Week3-Javascript/2.Tuesday/budget-tracker/app.js
+++ Week3-Javascript/2.Tuesday/budget-tracker/app.js
@@ -6,6 +6,9 @@ const amount = document.getElementById("amount");
 const category = document.getElementById("category");
 const expenseList = document.getElementById("expense-list");
 
+//This array will hold ALL my expenses
+let expenses = [];
+
 //Hey, if someone submits the form, let me know
 //Inside the parenthesis there are two arguments
 //first argument, what is the event?
@@ -22,7 +25,50 @@ function submitForm(event){
     const amountValue = amount.value;
     const categoryChoice = category.value;
 
-    console.log(description);
-    console.log(amountValue);
-    console.log(categoryChoice);
+    let expense = {
+        description: description,
+        amount: amountValue,
+        category: categoryChoice
+    }
+
+    //add expense to the array of expenses
+    expenses.push(expense);
+
+    console.log("Expenses array", expenses);
+
+    form.reset();
+    displayExpenses();
+}
+
+function displayExpenses(){
+    //clear the list before we write anything
+    expenseList.innerHTML = "";
+
+    //loop through the expenses array
+    for(let i = 0; i < expenses.length; i++){
+        let currentExpense = expenses[i];
+
+        //create an html element with JS
+        //This is like writing <li></li>
+        const li = document.createElement("li");
+
+        if(currentExpense.category === "Rent"){
+            //add a class to the li element
+            li.classList.add("rent-expense");
+        }
+        else if(currentExpense.category === "Entertainment"){
+            li.classList.add("entertainment-expense");
+        }
+        else{
+            li.classList.add("groceries-expense");
+        }
+
+        //fill it with data from the expense
+        //<li>description - amount - category</li>
+        li.textContent = `${currentExpense.description} - ${currentExpense.amount} - ${currentExpense.category}`;
+
+        //add it to the page
+        expenseList.appendChild(li);
+    }
+
 }
