ffbd739dcb65abbb634d22d26a4f83a32a27aa74
yithril <yithril@gmail.com>
2024-12-06 10:03:03 -0500

    change

--- advanced-api-example/src/main/java/com/example/our_first_api/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.our_first_api.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
